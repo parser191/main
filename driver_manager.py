1760d3d8b850c9286e60a224014518d217d106a6
fufic123 <semenovmark07@gmail.com>
2024-02-18 22:34:46 +0200

    d

--- driver_manager.py
+++ driver_manager.py
@@ -8,12 +8,12 @@ import random
 def create_driver(headless: bool, timeout: float , proxy: str = "") -> webdriver:
     options = webdriver.ChromeOptions()
     if(headless): 
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--incognito")
         options.add_argument('--blink-settings=imagesEnabled=false')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
         options.add_argument("--disable-extensions")
 
     seleniumwire_options = {}
